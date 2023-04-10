7e98455d03b1bf39c0506d748c582ad76e1db7ee
knguyen22 <knguyen22@hawk.iit.edu>
2023-04-10 11:40:06 -0500

    add dataStore

---
+++ src/DataStore/DS_1.java
@@ -0,0 +1,41 @@
+package DataStore;
+
+public class DS_1 extends DataStore{
+	private float temp_p;
+	private float temp_v;
+	private float price;
+	private float cf;
+	
+	public float getTemp_p() {
+		return this.temp_p;
+	}
+	
+	public void setTemp_p(float p) {
+		this.temp_p = p;
+	}
+	
+	public float getTemp_v() {
+		return this.temp_v;
+	}
+	
+	public void setTemp_v(float v) {
+		this.temp_v = v;
+	}
+	
+	public float getPrice() {
+		return this.price;
+	}
+	
+	public void setPrice(float p) {
+		this.price = p;
+	}
+	
+	public float getCF() {
+		return this.cf;
+	}
+	
+	public void setCF(float c) {
+		this.cf = c;
+	}
+
+}
---
+++ src/DataStore/DS_2.java
@@ -0,0 +1,42 @@
+package DataStore;
+
+public class DS_2 extends DataStore {
+	private float temp_p;
+	private int temp_v;
+	private float price;
+	private int cf;
+
+	public float getTemp_p() {
+		return this.temp_p;
+	}
+	
+	public void setTemp_p(float p) {
+		this.temp_p = p;
+	}
+	
+	public int getTemp_v() {
+		return this.temp_v;
+	}
+	
+	public void setTemp_v(int v) {
+		this.temp_v = v;
+	}
+	
+	public float getPrice() {
+		return this.price;
+	}
+	
+	public void setPrice(float p) {
+		this.price = p;
+	}
+	
+	public int getCF() {
+		return this.cf;
+	}
+	
+	public void setCF(int c) {
+		this.cf = c;
+	}
+	
+	
+}
