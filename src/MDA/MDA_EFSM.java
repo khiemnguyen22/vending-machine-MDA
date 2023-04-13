b3458d2026651680e426fa94d0df832592bfd1a5
knguyen22 <knguyen22@hawk.iit.edu>
2023-04-13 00:46:57 -0500

    fill out MDA_EFSM class

--- src/MDA/MDA_EFSM.java
+++ src/MDA/MDA_EFSM.java
@@ -1,5 +1,6 @@
 package MDA;
 import MDA.State.*;
+import OP.*;
 
 public class MDA_EFSM {
 	
@@ -9,44 +10,52 @@ public class MDA_EFSM {
 	private State S;
 	private State[] LS;
 	
-	public MDA_EFSM() {
+	public MDA_EFSM(OP op) {
+		
+		LS = new State[4];
+		
+		LS[0] = new Start(this,op);
+		LS[1] = new NoCups(this,op);
+		LS[2] = new Idle(this,op);
+		LS[3] = new CoinInserted(this,op);
+		
+		S = LS[0];
 		
 	}
-	
 	public void changeState(int s) {
 		S = LS[s];
 		System.out.println("New state: "+ S.toString());
 	}
 	
 	public void create() {
-		
+		S.create();
 	}
 	
 	public void insert_cups(int n) {
-		
+		S.insert_cups(n);
 	}
 	
 	public void coin(int f) {
-		
+		S.coin(f);
 	}
 	
 	public void card() {
-		
+		S.card();
 	}
 	
 	public void cancel() {
-		
+		S.cancel();
 	}
 	
 	public void set_price() {
-		
+		S.set_price();
 	}
 	
 	public void dispose_drink(int d) {
-		
+		S.dispose_drink(d);
 	}
 	
 	public void additive(int a) {
-		
+		S.additive(a);
 	}
 }
