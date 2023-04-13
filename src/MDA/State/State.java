4713a28594e7fa7ab1ad14ccc8b921c8bc250589
knguyen22 <knguyen22@hawk.iit.edu>
2023-04-12 22:01:39 -0500

    add state template

--- src/MDA/State/State.java
+++ src/MDA/State/State.java
@@ -2,6 +2,38 @@ package MDA.State;
 
 public class State {
 
+	public void create() {
+		
+	}
+	
+	public void insert_cups(int n) {
+		
+	}
+	
+	public void coin(int f) {
+		
+	}
+	
+	public void card() {
+		
+	}
+	
+	public void cancel() {
+		
+	}
+	
+	public void set_price() {
+		
+	}
+	
+	public void dispose_drink(int d) {
+		
+	}
+	
+	public void additive(int a) {
+		
+	}
+	
 	public String toString() {
 		return "Default state";
 	}
